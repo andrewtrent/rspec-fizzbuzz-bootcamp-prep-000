22176c8a8304ce157baadede8bca78f4c9cd6408
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-04 10:42:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(num)
-  if num.fdiv(3) == num.fdiv(3).to_i
-    "Fizz"
-  end
-  if num.fdiv(5) == num.fdiv(3).to_i
+  if num.fdiv(3) == num.fdiv(3).to_i && num.fdiv(5) == num.fdiv(5).to_i
+    "FizzBuzz"
+  elsif num.fdiv(5) == num.fdiv(5).to_i
     "Buzz"
+  elsif num.fdiv(3) == num.fdiv(3).to_i
+    "Fizz"
   else
     nil
   end
